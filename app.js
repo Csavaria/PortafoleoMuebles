58807a73ef6985ec67f8b964db78a29a85958784
cristIan <cristian2012sa@gmail.com>
2023-02-23 00:05:54 -0300

    fff

---
+++ app.js
@@ -0,0 +1,90 @@
+// // const post = [
+// //   { id: 1, nombre: "cristian" },
+// //   { id: 2, nombre: "pedro" },
+// //   { id: 3, nombre: "juan" },
+// // ];
+
+// // console.log(post.reduce((previous, current) => {
+// //     return current
+// // }, []));
+
+// // const saludar = (nombre) => {
+// //     return `hola ${nombre}`
+// // }
+
+// // console.log(saludar('cristian'));
+// // console.log(saludar());
+
+// // const a = "hey!";
+// // function global() {
+// //   const b = '!que'
+// //   function local() {
+// //     const c = 'tal/'
+// //     return a+b+c
+// //   }
+
+// //   return local
+// // }
+// // const clousure = global()
+// // console.log(clousure());
+
+// const data = [
+//   {
+//     id: 1,
+//     name: "Leanne Graham",
+//     username: "Bret",
+//     email: "Sincere@april.biz",
+//     address: {
+//       street: "Kulas Light",
+//       suite: "Apt. 556",
+//       city: "Gwenborough",
+//       zipcode: "92998-3874",
+//       geo: {
+//         lat: "-37.3159",
+//         lng: "81.1496",
+//       },
+//     },
+//     phone: "1-770-736-8031 x56442",
+//     website: "hildegard.org",
+//     company: {
+//       name: "Romaguera-Crona",
+//       catchPhrase: "Multi-layered client-server neural-net",
+//       bs: "harness real-time e-markets",
+//     },
+//   },
+//   {
+//     id: 2,
+//     name: "Ervin Howell",
+//     username: "Antonette",
+//     email: "Shanna@melissa.tv",
+//     address: {
+//       street: "Victor Plains",
+//       suite: "Suite 879",
+//       city: "Wisokyburgh",
+//       zipcode: "90566-7771",
+//       geo: {
+//         lat: "-43.9509",
+//         lng: "-34.4618",
+//       },
+//     },
+//     phone: "010-692-6593 x09125",
+//     website: "anastasia.net",
+//     company: {
+//       name: "Deckow-Crist",
+//       catchPhrase: "Proactive didactic contingency",
+//       bs: "synergize scalable supply-chains",
+//     },
+ 
+    
+//   },
+// ];
+
+// const getDatos = () => {
+//   setTimeout(() => {
+//     return data;
+//   }, 3000);
+  
+// };
+
+// console.log(getDatos());
+
